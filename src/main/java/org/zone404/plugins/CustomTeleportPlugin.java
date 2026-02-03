8837a689e7988e26413fb2ee81915e719ad0bb28
Nick <256194933+nickt128@users.noreply.github.com>
2026-02-02 17:59:37 -0700

    more aggressively disable/override built-in teleport plugin

--- src/main/java/org/zone404/plugins/CustomTeleportPlugin.java
+++ src/main/java/org/zone404/plugins/CustomTeleportPlugin.java
@@ -1,9 +1,29 @@
 package org.zone404.plugins;
 
+import com.hypixel.hytale.assetstore.event.LoadedAssetsEvent;
+import com.hypixel.hytale.assetstore.map.DefaultAssetMap;
 import com.hypixel.hytale.builtin.teleport.TeleportPlugin;
 import com.hypixel.hytale.builtin.teleport.Warp;
+import com.hypixel.hytale.builtin.teleport.commands.teleport.SpawnCommand;
+import com.hypixel.hytale.builtin.teleport.commands.teleport.TeleportCommand;
+import com.hypixel.hytale.builtin.teleport.commands.warp.WarpCommand;
+import com.hypixel.hytale.builtin.teleport.components.TeleportHistory;
+import com.hypixel.hytale.common.plugin.PluginIdentifier;
+import com.hypixel.hytale.common.plugin.PluginManifest;
+import com.hypixel.hytale.common.semver.SemverRange;
+import com.hypixel.hytale.event.EventRegistry;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.asset.type.model.config.ModelAsset;
+import com.hypixel.hytale.server.core.command.system.CommandRegistry;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.plugin.PluginBase;
+import com.hypixel.hytale.server.core.plugin.PluginManager;
+import com.hypixel.hytale.server.core.plugin.PluginState;
 import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.events.AddWorldEvent;
+import com.hypixel.hytale.server.core.universe.world.events.AllWorldsLoadedEvent;
+import com.hypixel.hytale.server.core.universe.world.events.ChunkPreLoadProcessEvent;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.util.BsonUtil;
 import org.bson.BsonArray;
 import org.bson.BsonDocument;
@@ -11,8 +31,12 @@ import org.bson.BsonDocument;
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
@@ -20,7 +44,10 @@ import java.util.logging.Level;
 
 /// Overrides `TeleportPlugin` and changes the save/load path to warps_hidden.json instead of warps.json.
 /// If a warps.json exists but not a warps_hidden.json the warps.json be moved to warps_hidden.json.
+@SuppressWarnings("unused")
 public class CustomTeleportPlugin extends TeleportPlugin {
+    @Nonnull
+    private static final HytaleLogger LOGGER = HiddenTeleportersPlugin.get().getLogger();
     @Nonnull
     private final ReentrantLock saveLock = new ReentrantLock();
     @Nonnull
@@ -28,6 +55,47 @@ public class CustomTeleportPlugin extends TeleportPlugin {
 
     public CustomTeleportPlugin(@Nonnull JavaPluginInit init) {
         super(init);
+        var tpPlugin = PluginManager.get().getPlugin(PluginIdentifier.fromString("Hytale:Teleport"));
+        disableTeleportPlugin((TeleportPlugin) tpPlugin);
+        if (tpPlugin != null && tpPlugin.isDisabled()) {
+            this.getLogger().at(Level.INFO).log("Hytale:Teleport disabled successfully.");
+        } else {
+            this.getLogger().at(Level.WARNING).log("Failed to disable Hytale:Teleport");
+        }
+
+        var teleporterPlugin = PluginManager.get().getPlugin(PluginIdentifier.fromString("Hytale:Teleporter"));
+        if (teleporterPlugin != null) {
+            var deps = new HashMap<>(teleporterPlugin.getManifest().getDependencies());
+            deps.remove(PluginIdentifier.fromString("Hytale:Teleport"));
+            setDependencies(teleporterPlugin, deps);
+            this.getLogger().at(Level.INFO).log("Successfully removed Hytale:Teleport dependency from Hytale:Teleporter.");
+        } else {
+            this.getLogger().at(Level.WARNING).log("Failed to replace Hytale:Teleport dependency");
+        }
+    }
+
+    @Nonnull
+    @Override
+    public HytaleLogger getLogger() {
+        return HiddenTeleportersPlugin.get().getLogger();
+    }
+
+    @Override
+    protected void setup() {
+        setInstance(this);
+        CommandRegistry commandRegistry = this.getCommandRegistry();
+        EventRegistry eventRegistry = this.getEventRegistry();
+        commandRegistry.registerCommand(new TeleportCommand());
+        commandRegistry.registerCommand(new WarpCommand());
+        commandRegistry.registerCommand(new SpawnCommand());
+        eventRegistry.register(LoadedAssetsEvent.class, ModelAsset.class, this::onModelAssetChange);
+        eventRegistry.registerGlobal(ChunkPreLoadProcessEvent.class, this::onChunkPreLoadProcess);
+        eventRegistry.registerGlobal(AddWorldEvent.class, CustomTeleportPlugin::onAddWorld);
+        eventRegistry.registerGlobal(AllWorldsLoadedEvent.class, (_) -> this.loadWarps());
+        this.setPrivateField("teleportHistoryComponentType", EntityStore.REGISTRY.registerComponent(TeleportHistory.class, TeleportHistory::new));
+        this.setPrivateField("warpComponentType", EntityStore.REGISTRY.registerComponent(WarpComponent.class, () -> {
+            throw new UnsupportedOperationException("WarpComponent must be created manually");
+        }));
     }
 
     @Override
@@ -99,5 +167,83 @@ public class CustomTeleportPlugin extends TeleportPlugin {
             this.postSaveRedo.set(true);
         }
 
+    }
+
+    public static void setInstance(CustomTeleportPlugin instance) {
+        try {
+            Field instanceField = TeleportPlugin.class.getDeclaredField("instance");
+            instanceField.setAccessible(true);
+            instanceField.set(null, instance);
+            LOGGER.at(Level.INFO).log("TeleportPlugin instance successfully overridden.");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            LOGGER.at(Level.SEVERE).log("Failed to override TeleportPlugin instance: " + e);
+        }
+    }
+
+    private void setPrivateField(String name, Object value) {
+        try {
+            Field field = TeleportPlugin.class.getDeclaredField(name);
+            field.setAccessible(true);
+            field.set(TeleportPlugin.get(), value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            LOGGER.at(Level.SEVERE).log("Failed to set private field: " + e);
+        }
+    }
+
+    private static void disableTeleportPlugin(TeleportPlugin plugin) {
+        try {
+            Field stateField = PluginBase.class.getDeclaredField("state");
+            stateField.setAccessible(true);
+            stateField.set(plugin, PluginState.DISABLED);
+            LOGGER.at(Level.INFO).log("TeleportPlugin disabled.");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            LOGGER.at(Level.SEVERE).log("Failed to disable TeleportPlugin: " + e);
+        }
+    }
+
+    public static void setDependencies(PluginBase plugin, Map<PluginIdentifier, SemverRange> dependencies) {
+        var manifest = plugin.getManifest();
+        try {
+            Field stateField = PluginManifest.class.getDeclaredField("dependencies");
+            stateField.setAccessible(true);
+            stateField.set(manifest, dependencies);
+            LOGGER.at(Level.INFO).log("Dependencies overridden successfully.");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            LOGGER.at(Level.SEVERE).log("Failed to disable TeleportPlugin: " + e);
+        }
+    }
+
+    private Method getPrivateMethod(String methodName, Class<?>...parameterTypes) throws NoSuchMethodException {
+        Method method = TeleportPlugin.class.getDeclaredMethod(methodName, parameterTypes);
+        method.setAccessible(true);
+        return method;
+    }
+
+    private void onModelAssetChange(@Nonnull LoadedAssetsEvent<String, ModelAsset, DefaultAssetMap<String, ModelAsset>> event) {
+        try {
+            Method method = this.getPrivateMethod("onModelAssetChange", LoadedAssetsEvent.class);
+            method.invoke(TeleportPlugin.get(), event);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            this.getLogger().at(Level.SEVERE).log("Failed call onModelAssetChange: " + e);
+        }
+    }
+
+    private void onChunkPreLoadProcess(@Nonnull ChunkPreLoadProcessEvent event) {
+        try {
+            Method method = this.getPrivateMethod("onChunkPreLoadProcess", ChunkPreLoadProcessEvent.class);
+            method.invoke(TeleportPlugin.get(), event);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            this.getLogger().at(Level.SEVERE).log("Failed call onChunkPreLoadProcess: " + e);
+        }
+    }
+
+    private static void onAddWorld(AddWorldEvent event) {
+        var markerProviders = event.getWorld().getWorldMapManager().getMarkerProviders();
+        if (markerProviders.get("warps") != null) {
+            LOGGER.at(Level.INFO).log("Removing warps marker provider.");
+            markerProviders.remove("warps");
+        } else {
+            LOGGER.at(Level.INFO).log("Warps marker provider not found.");
+        }
     }
 }
